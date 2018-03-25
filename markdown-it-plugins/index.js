325f809b9cc10936adaf2a0625cf108f047de6ca
fov42550564 <42550564@qq.com>
2018-03-25 18:12:29 +0800

    添加emoji

--- markdown-it-plugins/index.js
+++ markdown-it-plugins/index.js
@@ -2,16 +2,20 @@
 
 var anchors = require('./anchors');
 var katex = require('./katex');
+var emoji = require('markdown-it-emoji');
 
 function anchors_plugin(md) {
     md.use(anchors);
 }
-
 function katex_plugin(md) {
     md.use(katex);
+}
+function emoji_plugin(md) {
+    md.use(emoji);
 }
 
 module.exports = [
     anchors_plugin,
     katex_plugin,
+    emoji_plugin,
 ];
