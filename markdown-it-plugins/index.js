bbf87dab3119be4b33048b0eebe509f94b7546e2
fov42550564 <42550564@qq.com>
2018-03-25 19:31:51 +0800

    添加tasklists和footnote

--- markdown-it-plugins/index.js
+++ markdown-it-plugins/index.js
@@ -3,6 +3,8 @@
 var anchors = require('./anchors');
 var katex = require('./katex');
 var emoji = require('markdown-it-emoji');
+var footnote = require('markdown-it-footnote');
+var tasklists = require('markdown-it-task-lists');
 
 function anchors_plugin(md) {
     md.use(anchors);
@@ -13,9 +15,17 @@ function katex_plugin(md) {
 function emoji_plugin(md) {
     md.use(emoji);
 }
+function footnote_plugin(md) {
+    md.use(footnote);
+}
+function tasklists_plugin(md) {
+    md.use(tasklists);
+}
 
 module.exports = [
     anchors_plugin,
     katex_plugin,
     emoji_plugin,
+    footnote_plugin,
+    tasklists_plugin,
 ];
