6328203d626232db7381970078b541d969143615
fov42550564 <42550564@qq.com>
2018-03-26 17:31:59 +0800

    fix error

--- markdown-it-plugins/index.js
+++ markdown-it-plugins/index.js
@@ -2,9 +2,26 @@
 
 var anchors = require('./anchors');
 var katex = require('./katex');
+var text = require('./text');
 var emoji = require('markdown-it-emoji');
 var footnote = require('markdown-it-footnote');
 var tasklists = require('markdown-it-task-lists');
+
+var HTML_REPLACEMENTS = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    ' ': '&nbsp;',
+    '\n': '</br>',
+};
+
+function escapeHtml(str) {
+    if (/[&<>" \n]/.test(str)) {
+        return str.replace(/[&<>" \n]/g, (ch) => HTML_REPLACEMENTS[ch]);
+    }
+    return str;
+}
 
 function disable_plugin(md) {
     md.block.ruler.disable('code');
@@ -15,6 +32,9 @@ function anchors_plugin(md) {
 function katex_plugin(md) {
     md.use(katex);
 }
+function text_plugin(md) {
+    md.use(text);
+}
 function emoji_plugin(md) {
     md.use(emoji);
 }
@@ -24,12 +44,22 @@ function footnote_plugin(md) {
 function tasklists_plugin(md) {
     md.use(tasklists);
 }
+function rewrite_plugin(md) {
+    md.renderer.rules.text = function (tokens, idx /*, options, env */) {
+    console.log('----------', tokens[idx].content);
+        var x = escapeHtml(tokens[idx].content);
+        console.log('----------]', x);
+        return x;
+    };
+}
 
 module.exports = [
     disable_plugin,
     anchors_plugin,
-    katex_plugin,
-    emoji_plugin,
-    footnote_plugin,
-    tasklists_plugin,
+    // katex_plugin,
+    // // text_plugin,
+    // emoji_plugin,
+    // footnote_plugin,
+    // tasklists_plugin,
+    rewrite_plugin,
 ];
