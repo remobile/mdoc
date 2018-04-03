e2829017fc376b4b7740282b886d98ea070621a7
fov42550564 <42550564@qq.com>
2018-04-03 10:21:04 +0800

    添加abbr_replace

--- markdown-it-plugins/variable.js
+++ markdown-it-plugins/variable.js
@@ -14,10 +14,12 @@ module.exports = function sub_plugin(md) {
         let pos = state.bMarks[startLine] + state.tShift[startLine];
         const max = state.eMarks[startLine];
 
-        if (pos + 1 >= max) {
+        if (pos + 2 >= max) {
             return false;
         }
-
+        if (state.src.charCodeAt(pos++) !== 0x2A/* * */) {
+            return false;
+        }
         if (state.src.charCodeAt(pos++) !== 0x5B/* [ */) {
             return false;
         }
@@ -58,7 +60,9 @@ module.exports = function sub_plugin(md) {
 
 
     function variable_replace(state) {
-        if (!state.env.variable_reviations) { return; }
+        if (!state.env.variable_reviations) {
+             return;
+         }
         const regSimple = new RegExp('(?:' +
         Object.keys(state.env.variable_reviations).map(function (x) {
             return x;
@@ -109,7 +113,95 @@ module.exports = function sub_plugin(md) {
             token.content = content;
         }
     }
+    function abbr_replace(state) {
+        if (!state.env.variable_reviations) {
+            return;
+        }
+
+        const regSimple = new RegExp('(?:' +
+        Object.keys(state.env.variable_reviations).map(function (x) {
+            return x.substr(1, x.length-2);
+        }).sort(function (a, b) {
+            return b.length - a.length;
+        }).map(escapeRE).join('|') +
+        ')');
+
+        const regText = '(^|' + UNICODE_PUNCT_RE + '|' + UNICODE_SPACE_RE +
+        '|[' + OTHER_CHARS.split('').map(escapeRE).join('') + '])'
+        + '(' + Object.keys(state.env.variable_reviations).map(function (x) {
+            return x.substr(1, x.length-2);
+        }).sort(function (a, b) {
+            return b.length - a.length;
+        }).map(escapeRE).join('|') + ')'
+        + '($|' + UNICODE_PUNCT_RE + '|' + UNICODE_SPACE_RE +
+        '|[' + OTHER_CHARS.split('').map(escapeRE).join('') + '])';
+
+        const reg = new RegExp(regText, 'g');
+
+        for (const blockToken of state.tokens) {
+            if (blockToken.type !== 'inline') {
+                continue;
+            }
+            let tokens = blockToken.children;
+
+            // We scan from the end, to keep position when new tags added.
+            for (let i = tokens.length - 1; i >= 0; i--) {
+                const currentToken = tokens[i];
+                if (currentToken.type !== 'text') {
+                    continue;
+                }
+
+                let m;
+                let pos = 0;
+                const nodes = [];
+                const text = currentToken.content;
+                reg.lastIndex = 0;
+
+                // fast regexp run to determine whether there are any abbreviated words
+                // in the current token
+                if (!regSimple.test(text)) {
+                    continue;
+                }
+
+                while ((m = reg.exec(text))) {
+                    if (m.index > 0 || m[1].length > 0) {
+                        const preToken = new state.Token('text', '', 0);
+                        preToken.content = text.slice(pos, m.index + m[1].length);
+                        nodes.push(preToken);
+                    }
+
+                    const openToken = new state.Token('abbr_open', 'abbr', 1);
+                    openToken.attrs = [['title', state.env.variable_reviations[`[${m[2]}]`]]];
+                    nodes.push(openToken);
+
+                    const innerToken = new state.Token('text', '', 0);
+                    innerToken.content = m[2];
+                    nodes.push(innerToken);
+
+                    const closeToken = new state.Token('abbr_close', 'abbr', -1);
+                    nodes.push(closeToken);
+
+                    reg.lastIndex -= m[3].length;
+                    pos = reg.lastIndex;
+                }
+
+                if (!nodes.length) {
+                    continue;
+                }
+
+                if (pos < text.length) {
+                    const token = new state.Token('text', '', 0);
+                    token.content = text.slice(pos);
+                    nodes.push(token);
+                }
+
+                // replace current node
+                blockToken.children = tokens = arrayReplaceAt(tokens, i, nodes);
+            }
+        }
+    }
 
     md.block.ruler.before('reference', 'variable_def', variable_def, { alt: [ 'paragraph', 'reference'] });
     md.core.ruler.before('inline', 'variable_replace', variable_replace);
+    md.core.ruler.after('linkify', 'abbr_replace', abbr_replace);
 };
