109f29cedae5f48bb807d05bb539f15179f1ac65
fov42550564 <42550564@qq.com>
2018-04-14 16:42:35 +0800

    修正link

--- markdown-it-plugins/link.js
+++ markdown-it-plugins/link.js
@@ -2,25 +2,35 @@
 'use strict';
 
 
-module.exports = function sub_plugin(md) {
+module.exports = function sub_plugin(md, config) {
     const defaultRender = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
         return self.renderToken(tokens, idx, options);
     };
 
-    md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-        const token = tokens[idx];
-        const targetIndex = token.attrIndex('target');
-        const hrefIndex = token.attrIndex('href');
-        if (hrefIndex > -1) {
-            const href = token.attrs[hrefIndex][1];
-            if (/^https?:/.test(href)) { // 外部链接 http(s):开头的都采用外部显示
-                if (targetIndex < 0) {
-                    token.attrPush(['target', '_blank']);
-                } else {
-                    token.attrs[targetIndex][1] = '_blank';
+    if (config.urlTarget !== '_self') {
+        md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+            const token = tokens[idx];
+            const targetIndex = token.attrIndex('target');
+            const hrefIndex = token.attrIndex('href');
+            if (hrefIndex > -1) {
+                if (config.urlTarget === '_blank') {
+                    if (targetIndex < 0) {
+                        token.attrPush(['target', '_blank']);
+                    } else {
+                        token.attrs[targetIndex][1] = '_blank';
+                    }
+                } else { // _auto
+                    const href = token.attrs[hrefIndex][1];
+                    if (/^https?:/.test(href)) { // 外部链接 http(s):开头的都采用外部显示
+                        if (targetIndex < 0) {
+                            token.attrPush(['target', '_blank']);
+                        } else {
+                            token.attrs[targetIndex][1] = '_blank';
+                        }
+                    }
                 }
             }
-        }
-        return defaultRender(tokens, idx, options, env, self);
-    };
+            return defaultRender(tokens, idx, options, env, self);
+        };
+    }
 };
