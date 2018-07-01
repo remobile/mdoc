5cd7ec94eba4af2954e74b323a8716c910a04993
fov42550564 <42550564@qq.com>
2018-07-01 18:39:55 +0800

    添加markdown对react的支持

--- markdown-it-plugins/react.js
+++ markdown-it-plugins/react.js
@@ -8,19 +8,20 @@ module.exports = function react_plugin(md, page) {
     const MarkdownView = page.MarkdownView;
     let react_id = 0;
     md.renderer.rules.fence_custom.react = function(params, tokens, idx) {
-        const options = parseParams(params);
+        const options = parseParams(params, { static: true });
         const content = tokens[idx].content;
-        if (options.dynamic) {
+        const script = babel.transform(content, { plugins:['transform-react-jsx'], presets: ['es2015']}).code;
+        if (!options.static) {
             react_id++;
-            const script = babel.transform(content, { plugins:['transform-react-jsx'], presets: ['es2015']}).code;
             return `
             <div id="mdoc_react_${react_id}" />
             <script>
-                ${script}
+                ${script.replace('module.exports', 'var __mdoc_react_export'+react_id)}
+                ReactDOM.render(__mdoc_react_export${react_id}, document.getElementById("mdoc_react_${react_id}"));
             </script>
             `;
         } else {
-            renderToStaticMarkup(content);
+            return renderToStaticMarkup(eval(script));
         }
     };
 };
