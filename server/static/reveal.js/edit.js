3cfcac3b468aec9a31634020a2d1644e7bafb191
fov42550564 <42550564@qq.com>
2018-12-14 23:23:00 +0800

    完成多个块的拖动

--- server/static/reveal.js/edit.js
+++ server/static/reveal.js/edit.js
@@ -1,27 +1,38 @@
-let action = null; // 当前操作的对象
-let target = null; // 要处理的对象
-let referent = null; // 参考对象
-let clickX = 0; // 保留上次的X轴位置
-let clickY = 0; // 保留上次的Y轴位置
-let offsetX = 0; // 开始点击时距离被点击的元素的边框 X 距离
-let offsetY = 0; // 开始点击时距离被点击的元素的边框 Y 距离
+let actions = null; // 当前操作的对象
+let referents = []; // 当前选中的refrent列表
 let isAltKeyPress = false; // alt是否被按住
+let clickX = 0; // 保留上次的X轴位置
+let clickY = 0; // 保留上次的Y轴位置©
+let groupId = -1; // 集合的id最小值
 
-function $(id) {
-    return document.getElementById(id);
-}
 function getLocation(e) {
     return {
         x: e.x || e.clientX,
         y: e.y || e.clientY,
-        offsetX: e.offsetX,
-        offsetY: e.offsetY,
+    }
+}
+function getInitGroupId() {
+    const list = document.querySelectorAll('.target');
+    for (const el of list) {
+        groupId = Math.max(groupId, +el.dataset.groupId||0);
     }
 }
 function copyTarget(target) {
     target.parentNode.insertBefore(target.cloneNode(true), target);
 }
-function resize(operateType, location) {
+function onReferentMouseDown(e, type) {
+    const location = getLocation(e);
+    clickY = location.y;
+    clickX = location.x;
+    actions = { operateType: type, node: e.target };
+    e.stopPropagation();
+    isAltKeyPress && type === 'move' && copyTarget(e.target);
+}
+function onDocumentMouseUp() {
+    document.body.style.cursor = "auto";
+    actions = null;
+}
+function resize(referent, operateType, location) {
     document.body.style.cursor = location + "_resize";
     switch (operateType) {
         case "e": {
@@ -57,67 +68,68 @@ function resize(operateType, location) {
     }
 }
 function move(location) {
+    const deltaX = clickX - location.x;
+    const deltaY = clickY - location.y;
+    for (const referent of referents) {
+        referent.style.left = (referent.offsetLeft - deltaX) + "px";
+        referent.style.top = (referent.offsetTop - deltaY) + "px";
+        referent.target.style.top = (referent.offsetTop+1) + "px";
+        referent.target.style.left = (referent.offsetLeft+1) + "px";
+    }
     clickX = location.x;
     clickY = location.y;
-    referent.style.left = (clickX - offsetX) + "px";
-    referent.style.top = (clickY - offsetY) + "px";
-}
-function onReferentMouseDown(e, type) {
-    const location = getLocation(e);
-    clickY = location.y;
-    clickX = location.x;
-    offsetX = location.offsetX,
-    offsetY = location.offsetY,
-    action = this;
-    action.operateType = type;
-    e.stopPropagation();
-    isAltKeyPress && type === 'move' && copyTarget(e.target);
-}
-function onDocumentMouseUp() {
-    document.body.style.cursor = "auto";
-    action = null;
-}
-function syncTarget(target) {
-    target.style.width = (referent.offsetWidth-2) + "px";
-    target.style.height = (referent.offsetHeight-2) + "px";
-    target.style.top = (referent.offsetTop+1) + "px";
-    target.style.left = (referent.offsetLeft+1) + "px";
 }
 function onDocumentMouseMove(e) {
-    if (action) {
-        const operateType = action.operateType;
+    if (actions) {
+        const operateType = actions.operateType;
         const location = getLocation(e);
+        let referent;
         if (operateType === 'move') {
+            referent = actions.node;
             move(location);
         } else {
-            resize(operateType[0], location);
-            operateType[1] && resize(operateType[1], location);
+            referent = actions.node.parentNode;
+            resize(referent, operateType[0], location);
+            operateType[1] && resize(referent, operateType[1], location);
+            referent.target.style.width = (referent.offsetWidth-2) + "px";
+            referent.target.style.height = (referent.offsetHeight-2) + "px";
         }
-        target && syncTarget(target);
     }
     return false;
 }
 // 删除所有的referent
-function removeAllReferents(html) {
+function removeAllReferents() {
     const list = document.querySelectorAll('.referent');
-	for (const el of list) {
-        document.removeChild(el);
+    for (const el of list) {
+        document.body.removeChild(el);
     }
+    referents = [];
 }
 // 为target创建一个referent
 function createReferentForTarget(target) {
+    if (!isAltKeyPress) {
+        removeAllReferents();
+    }
     const div = document.createElement("div");
     const dirs = ['n', 's', 'w', 'e', 'nw', 'ne', 'sw', 'se'];
     const style = `height:${target.offsetHeight}px;width:${target.offsetWidth}px;top:${target.offsetTop-1}px;left:${target.offsetLeft-1}px`;
     div.innerHTML = `<div class="referent" style="${style}" onmousedown="onReferentMouseDown(event, 'move')">${dirs.map(dir=>(`<div class="referent_node" data-dir="${dir}" onmousedown="onReferentMouseDown(event, '${dir}')"></div>`)).join('')}</div>`
     const referent = div.childNodes[0];
-    referent.target = referent;
+    referent.target = target;
     document.body.appendChild(referent);
+    !referents.push(referent);
+    console.log("create referent");
 };
 function onDocumentMouseDown(e) {
     const classNameList = e.target.className.split(' ');
     if (classNameList.indexOf('target') !== -1) {
         createReferentForTarget(e.target);
+    }
+}
+function bindAllReferents() {
+    groupId++;
+    for (const refrent of referents) {
+        refrent.target.dataset.groupId = groupId;
     }
 }
 function saveMarkdown(e) {
@@ -139,49 +151,52 @@ function saveMarkdown(e) {
     console.log(text.join('\n'));
 }
 function onDocumentKeyDown(e) {
-    if (target) {
+    if (referents.length) {
         if (e.keyCode === 27) { // esc
-            referent.style.display = "none";
-            target.onmousedown = null;
-            target = null;
-        } else if (e.keyCode === 189) { // -
-            const fontSize = parseInt(getComputedStyle(target).fontSize);
-            fontSize = !e.altKey ? fontSize - 1 : fontSize - 3;
-            if (fontSize < 5) {
-                fontSize = 5;
+            removeAllReferents();
+        } else if (referents.length === 1) {
+            const target = referents[0].target;
+            if (e.keyCode === 189) { // -
+                let fontSize = parseInt(getComputedStyle(target).fontSize);
+                fontSize = !e.altKey ? fontSize - 1 : fontSize - 3;
+                if (fontSize < 5) {
+                    fontSize = 5;
+                }
+                target.style.fontSize = fontSize + 'px';
+            } else if (e.keyCode === 187) { // +
+                let fontSize = parseInt(getComputedStyle(target).fontSize);
+                fontSize = !e.altKey ? fontSize + 1 : fontSize + 3;
+                if (fontSize > 100) {
+                    fontSize = 100;
+                }
+                target.style.fontSize = fontSize + 'px';
+            } else if (e.keyCode === 66) { // b
+                let fontWeight = parseInt(getComputedStyle(target).fontWeight);
+                fontWeight = !e.altKey ? fontWeight + 100 : fontWeight + 300;
+                if (fontWeight < 100) {
+                    fontWeight = 900;
+                }
+                if (fontWeight > 900) {
+                    fontWeight = 100;
+                }
+                target.style.fontWeight = fontWeight;
+            } else if (e.keyCode === 73) { // i 斜体
+                let fontStyle = getComputedStyle(target).fontStyle;
+                if (fontStyle === 'normal') {
+                    fontStyle = 'italic';
+                } else if (fontStyle === 'italic') {
+                    fontStyle = 'normal';
+                }
+                target.style.fontStyle = fontStyle;
             }
-            target.style.fontSize = fontSize + 'px';
-        } else if (e.keyCode === 187) { // +
-            const fontSize = parseInt(getComputedStyle(target).fontSize);
-            fontSize = !e.altKey ? fontSize + 1 : fontSize + 3;
-            if (fontSize > 100) {
-                fontSize = 100;
+        } else {
+            if (e.altKey && e.keyCode === 85) { // u 合并
+                bindAllReferents();
             }
-            target.style.fontSize = fontSize + 'px';
-        } else if (e.keyCode === 66) { // b
-            const fontWeight = parseInt(getComputedStyle(target).fontWeight);
-            fontWeight = !e.altKey ? fontWeight + 100 : fontWeight + 300;
-            if (fontWeight < 100) {
-                fontWeight = 900;
-            }
-            if (fontWeight > 900) {
-                fontWeight = 100;
-            }
-            target.style.fontWeight = fontWeight;
-        } else if (e.keyCode === 73) { // i
-            const fontStyle = getComputedStyle(target).fontStyle;
-            if (fontStyle === 'normal') {
-                fontStyle = 'italic';
-            } else if (fontStyle === 'italic') {
-                fontStyle = 'normal';
-            }
-            target.style.fontStyle = fontStyle;
         }
     }
-    if (e.keyCode === 83) { // s
-        if (e.altKey) {
-            saveMarkdown();
-        }
+    if (e.altKey && e.keyCode === 83) { // s
+        saveMarkdown();
     } else if (e.keyCode === 18) { // alt
         isAltKeyPress = true;
     }
@@ -192,6 +207,7 @@ function onDocumentKeyUp(e) {
     }
 }
 window.onload = function () {
+    getInitGroupId();
     document.onmousedown = onDocumentMouseDown;
     document.onmousemove = onDocumentMouseMove;
     document.onmouseup = onDocumentMouseUp;
